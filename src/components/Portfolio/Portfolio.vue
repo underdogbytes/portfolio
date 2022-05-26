a4bf50e20cf51c34f592eddea7a14d77e3077340
bea <beatriztattooer@gmail.com>
2022-05-26 12:28:22 -0300

    💬 portfolio

--- src/components/Portfolio/Portfolio.vue
+++ src/components/Portfolio/Portfolio.vue
@@ -5,6 +5,7 @@ import PortfolioCarousel from './PortfolioCarousel.vue'
 <template>
     <section>
         <Title tag="portfolio" title="Stuff I made" />
+        <p>Hover the images to see more about each project ;)</p>
         <PortfolioCarousel />
         <p class="fun-fact">random but: did u know that the carousel above was made with pure CSS!? :D</p>
     </section>
