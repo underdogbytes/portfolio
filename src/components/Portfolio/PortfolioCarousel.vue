bdde605ae7874ef1ea9d527eaaf001f05b2b86f3
bea <beatriztattooer@gmail.com>
2022-05-26 16:47:12 -0300

    🐛 fix url

--- src/components/Portfolio/PortfolioCarousel.vue
+++ src/components/Portfolio/PortfolioCarousel.vue
@@ -55,14 +55,14 @@ export default {
                             title: 'Development',
                             description: 'The awesome designer Ito created the layout and I developed this website using HTML/CSS/JS',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/EagVHmlNpyIMhNtZC5seiQIBih6uxMkt53XqjmUIaIwdhj_ISQa7tVZiDF6B1quCVsuzY7bYCljrpLzJMcSyYQU8lNtZiiyvvL9mc4Ysa6Ghha93V3FZcmqk6HcznU2EOO-XqIIO2I4DEBCcPVG93Br9Fl_vV6IQkogrxINvi4p3xqcPHiMU4Iz_dqrwrymqz1PnTZPVC8N5U5qNQlWN0tbyq29_KB-5YFRVCdoUdMJsAmaUit43Oqyk2EOnIJIj8Zyoc_JOR_IGcw9EHdWKO-oU1PTm1HAe9AH8HGbrE1XiP6lSHY-cohz_nLogEmxMCxik2Ob9Xl3iVXFWfmhygI7b4s4dfQ6HX6DZRfKPoEzqzKG5qIPJnTBI5n5yczCaoJqlatXxrBMOKftfpUmZDhqAC2kww1klLa7QQ9b6iSlESV1foEc3OQIYv0FZpqsAqhPYFK1Bw4hgHgCpRpTnb5N38kWzL0AU3Fz1AEHFoYq0-x3xM35rSfvCfj6GlK6xvT7LDrg8KS2KVnF2WECmJGAuWDmHhVco8iSLVVhWbiBc9AjurpuKZShiTOAwqmvWtBiXCoyK6Dil-orETyf10e9wcXECrwTEHfVpf60oLYozYXu3VAIlnphieruKwgNBfDzCSLLwju-Jl5jLXahs1GBgFkLinHW4n2D7UIPx3Kaj1iOaixXzgmEjMmqTlsjQ0FexGFShuhwN6Z1Ty_aX2S-0BZ-uNZw0Ac5Wr6YlrQh1uu3LQgY0Ik6c8YIBN98=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSF0tGZ8jEAGAFbY5uYkKFOg81A_7czLioBbwhfsQtjMx6njdtad_iOKCA2IHOIIQr04kNU5rBPYQCTisjX3zqgWIqyee6h9nJMEbfBc0xeRb-mkCXxIl6e5EqMpIFdzb6vJRSxCBl5g11vhQ4VG29sYnp_dN3_ClgaAkRyOBIKEVhVPSV9o0QYkz4ig/s1080/tempestade-criativa.png',
                             alt: 'Tempestade Criativa'
                         },
                         {
                             title: 'Development',
                             description: 'The designer Bruno created the layout and I implemented this e-commerce using OpenCart. I also created this theme from zero :)',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/moA2VVmdNMVkvGHBXAM3nTgl0C-hUbMBpzLHyIH5h53U6JnfbeGlvPjARgbznCtP8MN0JXDk9OB9ri4_kjg2sK_NB_j0O4MyHg4vaSQgko3GY2CqqYjtFc010fI88g2DXYfUOnYri1eNCtlTaeWmMriaYNF5ussYhAP4sYa56SW6jvYi0FqFIckVe_OxqA5YFeCs7xRqS8EZZmCWqdUUa9OtZVf_AflqhbnHVk0a1dNO4QNiWfp9zQzuMEdR5RsHNS623PKrtRSfoavXobfW0spTHE02DWrPOgxXD8H3fCtklJUeCr9Ib6mBRQfOl9k2Fy2FklA5Sby9xX-dzbzAxpVZXlekyhE9l3E8uGR2FVJHNJ94Kun2cXilQFw1vY3r7zUTTaknwW6jEQbhDRUXgaqX8Ju8UDUZHuR4AAg8EIXxNlJGfF7F3FDf-8xjpR6IO1lcl-oORSyipPH-n3SBugV8XBVphskqH_cW3LYPamjgw3NwO6VmKh69vLJLLH7KSLo1mWQEe9NrL1Xi-mIibvsTkSXeqRbvHCXLucJhrw8ELAC_vtuUJ2anIAQ5UJcx99DBP8xtYrkiG60MLDbv66RchWxjFsz-1qRLDiI2iWNBs0KJO8WIQg3f_C8wW7sgpNUVJMUrCmgfrp69lLGfEGT9sS-yVWJynlIdmJQ5-F711xYRTFUrfJf0tkKQfbPRnu--3OlUHito9MMfYwdVDnCHGmuTxLyGE8MEr_uCCGw6F02Yj6XG9EiccquF2a0=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiKcvHF2xNuWb6NR1_mt2NUORGU6tg6vYmC0HlDSY4DiOUJZPVXrhmO1etB4IYVmMb1hXAHdTLyQn-fZt-sqm49EeaFy5nDrRs-oDp1ZjXYX38IxsUSzWSCx3IZ7VQmkcS5KaYv7JB-2tAFIAVKwGaHZdV03mDSLlcrbvD3-b6WNSZ2diCIFqWgx75z_Q/s1080/colecoes-fauna-do-brasil.png',
                             alt: 'Coleções Fauna do Brasil'
                         },
                     ]
@@ -76,14 +76,14 @@ export default {
                             title: 'Cat Signature',
                             description: 'This is an illustration made with Photoshop using a Wacom',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/npGycdzm0eulW8iKbgrjMe3s7SUL7d6suusmxB20keeJF8rLxtmyZjI8opY0iZBJjU9gNlYi7j5yQx3vyHqongSZRCPKtUirJZrppCMxwoYdXrGEnKCQdOSK_298-X_tmRJg6sdljugl9_nPBZhRCLltndcKQP0HOa83hkBByBgRAqAvWL1-2BxanpRTgnKJx1lTzDM9bYnOBoGSqVPviSD1QSebnrVKVQQ0pYN4Brvx8im3zBHbAX5U_eQkzD8rLIcyioXJF9cEJSvja58F2ajSJdkqBWP9ixY0trBm4PjdtrliYYRQdLpAw9wDY9YWuOAwoSgEsJLDdMT6RfS_K1Z0CoCQs4OKaWrLUsVTSb4p3yCxdjwz7b2imE7hwrRlhiBE9focEX3JoWld2EVNzQIxjb0JGwu6PZ-d7mNMiWSRS_F_xdWZwKZuB0ADe4Cmxu5kaUKY4ltJyGOtIEpSlS0x1osdrHXVGdCQM6VVGcQj0Fnh3Gw-YsIKrWJcISq3OFRnGjwjGqcGNJ38MCvg6mNnx06nOwLE1XyEaW6Dmc5iPwy_LGLH4_Mz7qxYX_HMloA7dd6RKLl2G6WfpmA0mMEIrq1v449LBkuwXq7ZAtTTEiNaxbUVPpR2aAvHpcYmVXAlYHMD03urJTzzCrZZjFdsN7aB69JQJE-J5LZmagquPx_J6JJt9uMmIQ1sSberGq3PwGiIcDfr4yHxLciMHaWwVefZftEX2QRQP_Xq2O9mQ66OjCVWb66T0aUy3sY=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjkpdCgY0x06bxq-j5ydCSUSEAF8LnMWxWXEfbn22zke6xItdz0a3cg8cQYWiEqB5xLk2VwGiFdKJvrJMwBgk8XafV54cvE_VkTE5AI6t9wCvLGRxuSwr3HYPRcqvctQI9traJFBK8qaZuPcTreyGBKtUg3RmPGzovxmHGH3fYvcbCNxzN2vDvJsV4KKA/s1080/cat-signature.png',
                             alt: 'Illustration: Cat Signature'
                         },
                         {
                             title: 'Show Gospel',
                             description: 'The design was from agency Publysh and I developed the website using PHP and Laravel',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/WOU5YeEZXJZBrjPhH3PIymkvTgW3wo7ZHcC6PFMNtA3duw8UMOeEn1CS_BqsAf9B0_CpPpDXvJxXt31-R6nViaO0IVUyj1BPik_H1vLokIU1FcJVhKlQadGXXRSnkCcANeDab6YTo5FPHdBosF9M4VcdnPJSAeoN7UVX1B_r1MZfz6RSEKVeRdRUFbE7lnr7L8PmVXfkkzIiLpm3WqTaR1qYYc42yqtyoWLRdYIslETrTLXStWfYsaxKvl73eA7cLxRFOAYnANmY-jFky_1GQREX9jEAUSNzNomF1ZvaMZISeH11hiktM6-ehpuB3XuMQI0WM0UL3dKW7pBQya088HOD2JT0DqMQjYmbFBDKkaX-5IBPMY0kpzNQTZgKdrpYK0qshNIj1PMYaGw9k9YsOTDFb_L73bBkk55L1WVT3pe8IXT6jhHnd1mt58johBlbkGANve5uE54CYFX_19wR65QB09vAuuj5gK_MATNkZ2RR5wDAvBllji4_zMBlFgcfQyrJ0-2KfpBfRH0YxTVmsgi6CmfaZD7w4KTt4D-mx9xlvmN-etVQn6v2iRj2hDsnFoRbV-Yv-CkMGM_3PWR2yDEjB_85K0ozzTTmjT2z6Tpoc1Ce61yUKVyMVLcpD8zmOTGwZV5lP6FLR2PgIHmEUlSeCgp0ftjUvstZTP9s7K9sJkCW4pvI7KP4NgY1Iug4yNPGB5UZiwbTIzwRPvLVfRVODZs9NsG2SzDgfYMcBy7tOhgLiBp-VuR5ih2wJSI=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj9rweV32qTGxJLpOCwL_gnRsieRlouPz6uPV6cDqwDKZZKVihtG5yd1Sg7dO8lMFBAWW4WY-OnOcdZvE12pZXGI0GYy_ehb6LWA0V0_7Gqd6uyXjQ7y2QYIkVn7CUshWyzQGngYm1Y8_1DbgimwsaN3uJNr8eMGW2srTaAktx-O8P_WfQ5VrKHzdgjkA/s1080/show-gospel.png',
                             alt: 'Show Gospel'
                         },
                     ]
@@ -97,14 +97,14 @@ export default {
                             title: 'Coming soon!',
                             description: 'Coming soon art made with Canva',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/R20Zfy5d6bZpslzmrogh8_sRypL1EWMMyo0KN3h-CfzW00H1AWhGeJE2gTmMgTlwC4xXIINMrEvXjqErlqj3cjJj50iHvR788HRPo0lJ2Wo7ScIM8TRsKUEJYcjXoTP6fDo1A715YGPcSJt9omV7dcD-tEoLkQQ2f_5QMni_QHZVeWeY8LFV0l9jWgWQzNvigvRYNIPiezsE7RO852_2CXGIVI1brp7TGMcv3pmoJpnMZvVCK7eYWx1ILulYxZBLDCvh0O24c-L4glQUrEVOnZoWkmU3Jdau_SvTCJu-48dW2hGlrRElB_rBbProdHX9OsfO3Q3YWbS3TtgbImkKmWce6zLbPTIXDNVa2h2yf5xxxWBCmiFq4apLgmXaLTNzdhpGmmv7JISfjbMmLJcztQHagbAsLP9wmz1g8aHsicMevNCZpKVu3cr7Wfw0wulLjbtUiTaD4tILHohI-Q1iZ0vVJfDwG70BUtwLDIPD5_je8W-obmq-7k6Exfj1wZnOrkWgowInIFYhV3Pd23hDwaWNov1lLG32YsapqSXloATnA_AF4V51xuByG19lLSm4Iq70y0H-L2sWBHFzNC3z-mXC0eVjwMc9F4C0SzgLDeOEMaD6LcGdJf1rSJzFiRqNh8iB4UyIeBnKW7soDfRUKqwOgMQUNooj7LUiPuhhaLHzEaLCaLr3aq_X_GbyeCD9MSl4faLKVj5tgK3F2pB7ngt9k-RvNpMkkt4Vo-16Msdlx44Xi4V360SZcSpH8ZM=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEht93C8qg3uUSR0LH_oFXQ0fzg3zOW35xURb41Sxi6CrY23ylMn_bz4kfa1S52SYOpn7cxyeZTxmYEW_kZG1EwwRLVY12s9SDRoEpgC1WZPs_QYWegoown8lPdJwPxL71HZZRA9uHxsITofY6YWzfD5CM-_b9Oea3BFgkrl_H450gLzHSnfhFDFHG-sPQ/s1080/coming-soon.png',
                             alt: 'Coming Soon'
                         },
                         {
                             title: 'Coming soon!',
                             description: 'Coming soon art made with Canva',
                             link: '',
-                            img: 'https://lh3.googleusercontent.com/R20Zfy5d6bZpslzmrogh8_sRypL1EWMMyo0KN3h-CfzW00H1AWhGeJE2gTmMgTlwC4xXIINMrEvXjqErlqj3cjJj50iHvR788HRPo0lJ2Wo7ScIM8TRsKUEJYcjXoTP6fDo1A715YGPcSJt9omV7dcD-tEoLkQQ2f_5QMni_QHZVeWeY8LFV0l9jWgWQzNvigvRYNIPiezsE7RO852_2CXGIVI1brp7TGMcv3pmoJpnMZvVCK7eYWx1ILulYxZBLDCvh0O24c-L4glQUrEVOnZoWkmU3Jdau_SvTCJu-48dW2hGlrRElB_rBbProdHX9OsfO3Q3YWbS3TtgbImkKmWce6zLbPTIXDNVa2h2yf5xxxWBCmiFq4apLgmXaLTNzdhpGmmv7JISfjbMmLJcztQHagbAsLP9wmz1g8aHsicMevNCZpKVu3cr7Wfw0wulLjbtUiTaD4tILHohI-Q1iZ0vVJfDwG70BUtwLDIPD5_je8W-obmq-7k6Exfj1wZnOrkWgowInIFYhV3Pd23hDwaWNov1lLG32YsapqSXloATnA_AF4V51xuByG19lLSm4Iq70y0H-L2sWBHFzNC3z-mXC0eVjwMc9F4C0SzgLDeOEMaD6LcGdJf1rSJzFiRqNh8iB4UyIeBnKW7soDfRUKqwOgMQUNooj7LUiPuhhaLHzEaLCaLr3aq_X_GbyeCD9MSl4faLKVj5tgK3F2pB7ngt9k-RvNpMkkt4Vo-16Msdlx44Xi4V360SZcSpH8ZM=s970-no?authuser=0',
+                            img: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEht93C8qg3uUSR0LH_oFXQ0fzg3zOW35xURb41Sxi6CrY23ylMn_bz4kfa1S52SYOpn7cxyeZTxmYEW_kZG1EwwRLVY12s9SDRoEpgC1WZPs_QYWegoown8lPdJwPxL71HZZRA9uHxsITofY6YWzfD5CM-_b9Oea3BFgkrl_H450gLzHSnfhFDFHG-sPQ/s1080/coming-soon.png',
                             alt: 'Coming Soon'
                         },
                     ]
