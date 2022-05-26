8f42750c883e25dbdcdd7ae4b71b72ea8b1a5132
bea <beatriztattooer@gmail.com>
2022-05-25 21:50:42 -0300

    ✨ parallax component

---
+++ src/components/Parallax.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="parallax" :style="`background-image: url(${backgroundImage});`"></div>
+</template>
+<script>
+export default {
+    props: ['backgroundImage']
+}
+</script>
+<style scoped>
+.parallax{
+    min-height: 500px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+@media screen and (max-width: 1024px){
+    .parallax{
+        background-attachment: scroll;
+    }
+}
+</style>
