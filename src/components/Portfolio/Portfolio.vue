c9c42971b95968ad51f83127ff5721ef94b43807
bea <beatriztattooer@gmail.com>
2022-05-31 17:41:28 -0300

    ✨ portfolio: new component!

--- src/components/Portfolio/Portfolio.vue
+++ src/components/Portfolio/Portfolio.vue
@@ -1,12 +1,17 @@
 <script setup>
 import Title from '../Title.vue'
-import PortfolioCarousel from './PortfolioCarousel.vue'
+import PortfolioCards from './PortfolioCards.vue';
 </script>
 <template>
     <section>
         <Title tag="portfolio" title="Stuff I made" />
-        <p>Hover the images to see more about each project ;)</p>
-        <PortfolioCarousel />
-        <p class="fun-fact">random but: did u know that the carousel above was made with pure CSS!? :D</p>
+        <p class="portfolio__description">Hover the images to see more about each project ;)</p>
+        <PortfolioCards />
     </section>
 </template>
+
+<style>
+.portfolio__description{
+    margin: 5em 0;
+}
+</style>
