38a6aa9a7b40bc2255f76e37a9c8d87b72d9a734
bea <beatriztattooer@gmail.com>
2022-06-13 17:41:57 -0300

    💄🍱♿️⚡️ dark mode is on!!! :D

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import ThemeToggle from './components/ThemeToggle.vue';
 </script>
 
 <template>
+  <ThemeToggle />
   <RouterView />
 </template>
 
