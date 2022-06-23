f30536cdcd5fe3fa606fed8fade8cc590d9f1940
bea <beatriztattooer@gmail.com>
2022-06-23 14:45:38 -0300

    💄 welcome

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,5 @@
 <template>
-    <header>
+    <div class="header">
         <div class="header__text">
             <div>
                 <p class="header__title">btrz</p>
@@ -20,13 +20,14 @@
                 </div>
             </div>
         </div>
-        <img loading="lazy" class="header__img" src="@/assets/welcome-img.png" alt="">
-    </header>
-    <div class="header__bg"></div>
+        <!-- <img loading="lazy" class="header__img" src="@/assets/welcome-img.png" alt=""> -->
+        
+        <div class="header__bg"></div>
+    </div>
 </template>
 <style scoped>
-header{
-    position: relative;
+.header{
+    position: initial;
     min-height: 100vh;
 }
 .header__bg{
@@ -38,15 +39,15 @@ header{
     background-image: url(@/assets/me.jpg);
     background-size: cover;
     background-position: center;
-    filter: blur(0.6em) grayscale(50%);
+    filter: blur(0.1em) grayscale(20%);
 }
 .header__text{
     position: sticky;
-    top: 10%;
-    left: 30%;
+    top: 25%;
+    left: 10%;
     margin: 3em;
     padding: 0 0 3em 0;
-    max-width: 400px;
+    max-width: 600px;
     opacity: 0.9;
     text-shadow:
         0 0 0.125em hsl(0 0% 100% / 0.3),
@@ -59,16 +60,18 @@ header{
         0 0 0.45em rgb(255, 36, 178);
 }
 .header__title{
-    font-size: 8rem;
+    font-size: clamp(3rem, 10vw, 16rem);
     font-weight: bold;
     color: #000000;
 }
+.header__title[data-theme='dark']{
+    color: pink;
+}
 .header__subtitle{
-    font-size: 3rem;
+    font-size: clamp(3rem, 4vw, 10rem);
     font-weight: bold;
     color: rgb(255, 255, 255);
 }
-.header__img{ display: none; }
 
 .header__social{
     margin: 2em 0 0 0;
@@ -79,34 +82,31 @@ header{
     max-width: 3em;
     margin: auto;
 }
-.header__social__icon.desktop{display: none;}
+.header__social__icon.desktop{ display: none; }
 
-@media screen and (min-width: 1000px){
+@media screen and (min-width: 900px){
     .header__text{
         position: absolute;
-        left: 30%;
-        top: 25%;
-        max-width: 400px;
+        left: 20%;
     }
-    .header__title{ font-size: 12rem; }
-    .header__subtitle{ font-size: 3.5rem; }
-    .header__img{
-        display: block;
+}
+@media screen and (min-width: 1000px){
+    .header__bg{
         position: absolute;
         top: 0;
         right: 0;
-        shape-outside: url(../assets/welcome-img.png);
         z-index: -1;
-        max-width: 40vw;
+        width: 100px;
+        min-width: 50%;
+        max-width: 100%;
+
+        filter: none;
+        background-image: url(../assets/welcome-img.png);
+        shape-outside: url(../assets/welcome-img.png);
     }
     .header__social__icon.mobile{display: none;}
     .header__social__icon.desktop{display: block;}
-    .header__bg{
-        display: none;
-    }
     .header__social{
-        margin: 2em 0 0 0;
-        column-width: 4em;
         column-rule: 1px solid rgba(31, 31, 31, 0.25);
     }
 }
