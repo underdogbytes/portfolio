7b9565cadff93a8b453ad8ea895f86efe86b07a5
bea <beatriztattooer@gmail.com>
2022-06-23 14:46:25 -0300

    ♻️ home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import Title from '../components/Title.vue'
 import Blog from '../components/Blog/Blog.vue'
-import Footer from '../components/Footer.vue'
 import Portfolio from '../components/Portfolio/Portfolio.vue';
 import Welcome from '../components/Welcome.vue';
 import ServicePrice from '../components/ServicePrice.vue';
+import Navbar from '../components/Navbar.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
+    <Navbar />
     <Welcome />
     <Portfolio />
     <ServicePrice />
