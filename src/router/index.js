362e1d031f3fa5d163dd33af88197c2183d32104
bea <beatriztattooer@gmail.com>
2022-06-23 15:18:34 -0300

    🚚 ✨ new page: projects
    1. created view
    2. created route

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ContactView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProjectsView.vue')
     }
   ]
 })
