2e81e57f801b7d70b24663c21b06451d9810b427
bea <beatriztattooer@gmail.com>
2024-02-02 17:04:30 -0300

    :sparkles: component[toggle]
    yeah yeah, isn't cute that it's literally a sun and a moon? :3333

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import ThemeToggle from '@/components/ThemeToggle.vue';
 </script>
 
 <template>
+	<ThemeToggle />
 	<RouterView />
 </template>
