e8b7ed30fbf7a304e6ef1fad4188aa71a43e37c4
bea <beatriztattooer@gmail.com>
2024-02-07 17:35:48 -0300

    :bug: fix[home]: imgs at prod still not appearing V

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,27 +1,27 @@
 const projects = [
   {
-    source: './src/assets/images/projects/colecoes-fauna-do-brasil.png',
+    source: '@/assets/images/projects/colecoes-fauna-do-brasil.png',
     name: 'Coleções: Fauna do Brasil',
     tags: ['developer'],
     techs: ['php', 'openCart'],
     description: 'Design by Bruno and developed with openCart by me.',
   },
   {
-    source: './src/assets/images/projects/renovacao.png',
+    source: '@/assets/images/projects/renovacao.png',
     tags: ['developer', 'designer'],
     techs: ['php', 'laravel'],
     name: 'Escola Renovação',
     description: 'Design and development by me using Figma and Laravel/PHP'
   },
   {
-    source: './src/assets/images/projects/tempestade-criativa.png',
+    source: '@/assets/images/projects/tempestade-criativa.png',
     tags: ['developer'],
     techs: ['html/css/js'],
     name: 'Agência Tempestade Criativa',
     description: 'Design by the talented Ito and developed with HTML/CSS/JS.'
   },
   {
-    source: './src/assets/images/projects/show-gospel.png',
+    source: '@/assets/images/projects/show-gospel.png',
     tags: ['developer'],
     techs: ['php', 'laravel'],
     name: 'Revista Show Gospel',
