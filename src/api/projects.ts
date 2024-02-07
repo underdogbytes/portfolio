d049d6173535745b92ddc6fc87a0e285094b2fd9
bea <beatriztattooer@gmail.com>
2024-02-07 17:43:42 -0300

    :bug: fix[home]: imgs at prod still not appearing VI
    running out of ideas, so teeeell me why this not wooorkssssss

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,27 +1,32 @@
+import fauna from '@/assets/images/projects/colecoes-fauna-do-brasil.png';
+import renovacao from '@/assets/images/projects/renovacao.png';
+import tempestade from '@/assets/images/projects/tempestade-criativa.png';
+import showGospel from '@/assets/images/projects/show-gospel.png';
+
 const projects = [
   {
-    source: '@/assets/images/projects/colecoes-fauna-do-brasil.png',
+    source: fauna,
     name: 'Coleções: Fauna do Brasil',
     tags: ['developer'],
     techs: ['php', 'openCart'],
     description: 'Design by Bruno and developed with openCart by me.',
   },
   {
-    source: '@/assets/images/projects/renovacao.png',
+    source: renovacao,
     tags: ['developer', 'designer'],
     techs: ['php', 'laravel'],
     name: 'Escola Renovação',
     description: 'Design and development by me using Figma and Laravel/PHP'
   },
   {
-    source: '@/assets/images/projects/tempestade-criativa.png',
+    source: tempestade,
     tags: ['developer'],
     techs: ['html/css/js'],
     name: 'Agência Tempestade Criativa',
     description: 'Design by the talented Ito and developed with HTML/CSS/JS.'
   },
   {
-    source: '@/assets/images/projects/show-gospel.png',
+    source: showGospel,
     tags: ['developer'],
     techs: ['php', 'laravel'],
     name: 'Revista Show Gospel',
