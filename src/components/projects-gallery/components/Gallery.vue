bb46b32353386a291bb1c80297b865ca66ab125c
bea <beatriztattooer@gmail.com>
2024-02-20 23:29:18 -0300

    :art: :construction: estamos entrando no estado da arte em algumas partes

--- src/components/projects-gallery/components/Gallery.vue
+++ src/components/projects-gallery/components/Gallery.vue
@@ -13,9 +13,22 @@
 				>
 			</div>
 
-			<div class="project-gallery__description">
-				<p class="project-gallery__title">{{ project.name }}</p>
-				<p class="project-gallery__type">{{ project.type }}</p>
+			<div class="project-gallery__text-box">
+				<p class="project-gallery__type">
+					{{ project.keys[0] }}, {{ project.date }}
+				</p>
+
+				<p class="project-gallery__role">
+					worked as
+					<span
+						v-for="(role, i) in project.workedAs"
+						:key="i"
+					>
+						<span v-if="i+1 == project.workedAs.length && project.workedAs.length != 1">and </span>
+						{{ role }}
+						<span v-if="i+2 < project.workedAs.length">, </span>
+					</span>
+				</p>
 			</div>
 		</div>
 	</div>
@@ -31,15 +44,18 @@ export default {
 
 <style>
 :root {
-	--projects-gallery-font-clr: #fff;
+	--projects-gallery-font-clr: #4a4a48;
+	--projects-gallery-box-clr: #fff;
 }
 
 [data-theme='light'] {
-	--projects-gallery-font-clr: #fff;
+	--projects-gallery-font-clr: #4a4a48;
+	-projects-gallery-box-clr: #fff;
 }
 
 [data-theme='dark'] {
 	--projects-gallery-font-clr: #4a4a48;
+	-projects-gallery-box-clr: #fff;
 }
 
 .project-gallery__grid {
@@ -49,7 +65,12 @@ export default {
 }
 
 .project-gallery__box {
+	padding: 20px;
+
 	border: 3px solid transparent;
+	border-radius: 8px;
+	background-color: var(--projects-gallery-box-clr);
+
 	cursor: pointer;
 }
 
@@ -70,20 +91,24 @@ export default {
 	transform: scale(1.2);
 }
 
-.project-gallery__description {
-	padding: 0.5rem;
-	font-size: 14px;
+.project-gallery__text-box {
+	padding-top: 0.5rem;
 }
 
-p.project-gallery__title {
-	color: var(--projects-gallery-font-clr);
-	font-size: medium;
-	font-weight: 800;
-	line-height: 1.1;
+p.project-gallery__type {
+	margin: 0;
+
+	font-family: var(--font-beanie);
+	font-size: 32px;
 }
 
 .project-gallery__type {
 	font-weight: 400;
+	color: var(--projects-gallery-font-clr);
+}
+
+.project-gallery__role {
+	font-size: 14px;
 	color: var(--projects-gallery-font-clr);
 }
 </style>
---
+++ src/views/open-letter/Index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<h1>OPEN LETTER</h1>
+		<p>
+			I'm a software engineer with over five years of experience, an avid learner with strong creative and analytical skills and blablabla.
+		</p>
+
+		<p>
+			Just between us, for real no cap bro:
+			I'm f*cking tired of this rat race and I don't give a damn about the values of your unique big company, project and/or brand that in the ends just makes the rich even richer while the laborer ones just get sick and burnout.
+		</p>
+		<p>
+			Unless you're a real person, with real problems, or are really trying to help the humanity, I'm not interest working with you. I f*cking love create solutions and I promissed to myself in my last burnout that I'll not develop one single line of code or a drawing for projects that I don't really believe.
+		</p>
+
+		<p>
+			I'll not allow this sick world turn off my creativity again.
+		</p>
+
+		<p>
+			And of course, I care about my own projects and personal life. Oh, and I like to draw and sharks too :)
+		</p>
+
+		<p>
+			Byyyeee ♡
+		</p>
+	</div>
+</template>
+<script>
+export default {
+	name: 'OpenLetter'
+}
+</script>
