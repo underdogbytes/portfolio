6db381b70ecc8b88b8bef38a55600c0139308746
bea <beatriztattooer@gmail.com>
2024-03-03 11:12:39 -0300

    :bug: olha, é um saco configurar em prod viu :p

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Underdog Bytes</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -32,23 +32,41 @@
             @endif
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
-
                 <div class="mt-16">
-                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
-                        ( ＾◡＾)っ Portfolio Under Maintenance ≽^•⩊•^≼
-                    </h2>
+                    <div class="grid grid-cols-1">
+                        <span
+                            class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
+                            >
+                            <div>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                                     ( ＾◡＾)っ UNDER MAINTENANCE ≽^•⩊•^≼
+                                </h2>
 
-                    <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                    Come back soon! :D
-                    </p>
+                                <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                    Come back soon :D
+                                </p>
+                            </div>
+                        </span>
+                    </div>
                 </div>
 
-                <div class="flex justify-center mt-16 px-0">
-                    <div class="text-center text-sm sm:text-left">
-                        &nbsp;
+                <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+                    <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
+                        <div class="flex items-center gap-4">
+                            <a
+                                href="https://github.com/tocrossbridge"
+                                target="_blank"
+                                class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                </svg>
+                                Github
+                            </a>
+                        </div>
                     </div>
 
-                    <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
+                    <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
