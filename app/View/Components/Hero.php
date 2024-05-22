d15a816446fe0b07517633a1defc8a2cad18bc14
bea <beatriztattooer@gmail.com>
2024-05-22 18:17:22 -0300

    :fire: remove default comments

--- app/View/Components/Hero.php
+++ app/View/Components/Hero.php
@@ -8,17 +8,11 @@ use Illuminate\View\Component;
 
 class Hero extends Component
 {
-    /**
-     * Create a new component instance.
-     */
-    
     public function __construct(public $bg)
     {
+        //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.hero');
--- app/View/Components/portfolio/galleryItem.php
+++ app/View/Components/portfolio/galleryItem.php
@@ -8,17 +8,11 @@ use Illuminate\View\Component;
 
 class galleryItem extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct(public $imgUrl, public $link)
     {
         //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.portfolio.gallery-item');
