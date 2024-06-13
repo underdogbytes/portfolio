3448fa8cf061207be541ba88897f553b5a5fbb50
bea <beatriztattooer@gmail.com>
2024-06-13 16:53:12 -0300

    :bug: fix: component import idk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,9 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $projectsJson = file_get_contents(public_path('data/best-projects.js'));
-        $cleanedContent = preg_replace('/[\x00-\x1F\x7F]/', '', $projectsJson);
-        $projects = json_decode($projectsJson, true);
-        return view('home', ['projects'=> $projects]);
+        return view('home');
     }
 }
--- app/View/Components/portfolio/BestProjects.php
+++ app/View/Components/portfolio/BestProjects.php
@@ -2,22 +2,38 @@
 
 namespace App\View\Components\portfolio;
 
-use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\File;
 use Illuminate\View\Component;
 
 class BestProjects extends Component
 {
-    public function __construct()
+    public $projects;
+    
+    public function __construct($projects = [])
     {
-        //
+        try {
+            // Tente ler o arquivo JSON
+            $projectsJson = File::get(public_path('data/best-projects.js'));
+            
+            // Decodificação do JSON
+            $cleanedContent = preg_replace('/[\x00-\x1F\x7F]/', '', $projectsJson);
+            $projects = json_decode($cleanedContent, true);
+
+            // Verificação de erros na decodificação
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new \Exception('Erro ao decodificar JSON em BestProjects: ' . json_last_error_msg());
+            }
+
+            $this->projects = $projects;
+        } catch (\Exception $e) {
+            \Log::error('Erro ao ler o arquivo JSON em BestProjects: ' . $e->getMessage());
+            $this->projects = [];
+        }
     }
 
-    public function render(): View|Closure|string
+    public function render(): View|string
     {
-        $projectsJson = file_get_contents(public_path('data/best-projects.js'));
-        $cleanedContent = preg_replace('/[\x00-\x1F\x7F]/', '', $projectsJson);
-        $projects = json_decode($projectsJson, true);
-        return view('components.portfolio.best-projects', ['projects'=> $projects]);
+        return view('components.portfolio.best-projects', ['projects' => $this->projects]);
     }
 }
