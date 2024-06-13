e704b671f500d1cd85e89279a854b4d1e80dec1d
bea <beatriztattooer@gmail.com>
2024-06-13 16:20:18 -0300

    :bug: fix: component import idk

--- app/View/Components/portfolio/BestProjects.php
+++ app/View/Components/portfolio/BestProjects.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class BestProjects extends Component
+class bestProjects extends Component
 {
     public $projects;
 
