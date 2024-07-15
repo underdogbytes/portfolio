e63c8de30d871221b11a9aade730eef522153eee
bea <beatriztattooer@gmail.com>
2024-07-15 19:09:13 -0300

    fix: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -5,7 +5,7 @@
             Underdog Bytes - 2024
         </a>
         <p>
-            developed with
+            desenvolvido com
             Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
         </p>
     </div>
