593df393a65de55cfeabf7167175b2128d393f05
bea <beatriztattooer@gmail.com>
2024-07-15 19:09:41 -0300

    fix[portfolio][show]: content

--- resources/views/pages/portfolio/show.blade.php
+++ resources/views/pages/portfolio/show.blade.php
@@ -13,13 +13,41 @@
     <x-utils.breadcrumb :breads="$breads" />
     <x-portfolio.head-item :info="$project['head']" />
 
+    {{-- PESQUISA: --}}
+    @if(isset($project['body']['research']) && $project['body']['research'])
+    <div class="m-5-auto">
+      <h2>{!! $project['body']['research']['title'] !!}</h2>
+      {!! $project['body']['research']['text'] !!}
+    </div>
+    @endif
+
+    {{-- ESCOLHA DO LAYOUT: --}}
+    @if(isset($project['body']['layout']) && $project['body']['layout'])
+    <div class="m-5-auto">
+      <h2>{!! $project['body']['layout']['title'] !!}</h2>
+      {!! $project['body']['layout']['text'] !!}
+    </div>
+    @endif
+
+    {{-- RESULTADO FINAL --}}
+    @if(isset($project['body']['result']) && $project['body']['result'])
+    <div class="m-5-auto">
+      <h2>{{ $project['body']['result']['title'] }}</h2>
+      @foreach ($project['body']['result']['imgs'] as $img)
+       <img src={{ asset($img) }} width="250" />   
+      @endforeach
+    </div>
+    @endif
+
+    {{-- FIGMA --}}
     @if(isset($project['body']['figmaPrototype']) && $project['body']['figmaPrototype'])
     <div class="m-5-auto">
-      <h2>Protótipo Figma</h2>
-
+      <h2>Figma</h2>
+    
       <iframe class="portfolio__proto_figma" src="{{ $project['body']['figmaPrototype']}}" allowfullscreen></iframe>
     </div>
     @endif
   </div>
+  
 </section>
 @endsection
