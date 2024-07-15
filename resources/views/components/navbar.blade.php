8ff1825d506f676cb12ebc75a0a3d648b6f3d84b
bea <beatriztattooer@gmail.com>
2024-07-15 16:39:32 -0300

    feat: navbar icons

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -10,7 +10,21 @@
             <a href="{{ url('/') }}">Home</a>
             <a href="{{ url('/portfolio') }}">Portfolio</a>
             <a href="{{ url('/contato') }}">Contato</a>
-            {{-- <a href="{{ url('/apoio') }}">≽^•⩊•^≼</a> --}}
+            <a href="{{ url('https://www.linkedin.com/in/btrz/') }}" target="_blank">
+                <div class="navbar__icon navbar__icon--linkedin"></div>
+            </a>
+            <a href="{{ url('https://www.behance.net/bridgetocross') }}" target="_blank">
+                <div class="navbar__icon navbar__icon--behance"></div>
+            </a>
+            <a href="{{ url('https://patreon.com/btrz') }}" target="_blank">
+                <div class="navbar__icon navbar__icon--patreon"></div>
+            </a>
+            <a href="{{ url('https://github.com/underdogbytes') }}" target="_blank">
+                <div class="navbar__icon navbar__icon--github"></div>
+            </a>
+            <a href="{{ url('https://www.instagram.com/bridgetocross') }}" target="_blank">
+                <div class="navbar__icon navbar__icon--instagram"></div>
+            </a>
         </div>
     </section>
 </nav>
