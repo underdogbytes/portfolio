754a216e344cd8814f33d87e406a320601aa0be1
bea <beatriztattooer@gmail.com>
2024-09-17 15:06:04 -0300

    :zap: feat[figma]: improving UX reading

--- resources/views/components/portfolio/figma.blade.php
+++ resources/views/components/portfolio/figma.blade.php
@@ -1,4 +1,9 @@
 <div class="m-5-auto">
   <h2>{!! __('projects.tools_figma') !!}</h2>
+  <p>
+    Clique no protótipo abaixo para ver os links navegáveis.
+    Caso o protótipo esteja descentralizado, clique na área do protótipo e aperte Z para redimensionar a visualização.
+    Você também pode apertar R para reiniciar o protótipo do projeto ou navegar entre as telas apertando as setas de navegação.
+  </p>
   <iframe class="portfolio__proto_figma" src="{{ $link }}" allowfullscreen></iframe>
 </div>
