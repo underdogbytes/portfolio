d5c321ded0804633d953ffb0765a066ca94b0dba
bea <beatriztattooer@gmail.com>
2024-09-17 15:07:27 -0300

    :speech_balloon: update projects lang

---
+++ lang/en/projects.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Projects
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used at show item of portfolio.
+    |
+    */
+
+    // Head
+    'head_description' => 'Descrição',
+    'head_links' => 'Links',
+    'head_tools_used' => 'Ferramentas utilizadas no projeto',
+    'website' => 'Website em ambiente de produção',
+
+    // Tools
+    'tools_behance' => 'Behance',
+    'tools_canva' => 'Canva',
+    'tools_competitor_analysis' => 'Análise de Concorrentes',
+    'tools_figma' => 'Figma',
+    'tools_laravel' => 'Laravel',
+    'tools_nielsen_heuristics' => 'Heurísticas de Nielsen',
+    'tools_vuejs' => 'VueJs',
+    'tools_user_survey' => 'Pesquisa com Usuários',
+    'tools_photoshop' => 'Photoshop',
+    'tools_php' => 'PHP',
+    'tools_opencart' => 'OpenCart',
+
+    // Roles
+    'roles_title' => 'Atuação:',
+    'roles_social_media_designer' => 'Designer Social Media',
+    'roles_UIUX' => 'Designer UI/UX',
+    'roles_dev_back' => 'Back-end',
+    'roles_dev_front' => 'Front-end',
+    'roles_dev_fullstack' => 'Full-stack',
+
+    // Project: Autismo em Mulheres
+    'autism_women' => 'Autismo em Mulheres',
+    'autism_women_description' => 'Conteúdo de Instagram criado para conscientização sobre autismo em mulheres utilizando as artes do jogo Don\'t Starve Together como ilustrações.',
+    'autism_women_research_title' => '01. Pesquisa',
+    'autism_women_research_description' => 'Para a pesquisa os principais artigos que basearam a criação do conteúdo foram: <ul><li>autism.org.uk/advice-and-guidance/what-is-autism/autistic-women-and-girls</li><li>med.stanford.edu/news/all-news/2022/02/autism-brain-sex-differences.html</li></ul>',
+    'autism_women_layout_title' => '02. Definição de Layout',
+    'autism_women_layout_description' => 'Com a pesquisa em mãos e uma rede social escolhida (instagram), optei por criar um carousel para comportar melhor o texto sobre mascaramento e esteriótipos; além de um último para colocar as referências.',
+    'autism_women_result_title' => '03. Projeto finalizado',
+
+    // Project: Escola Renovação
+    'escola_renovacao' => 'Escola Renovação',
+    'escola_renovacao_description' => 'A escola Renovação em São José dos Pinhais atende crianças e adolescentes. Foram duas etapas: prototipação navegável com Figma e, após aprovação da interface, o desenvolvimento com Laravel.',
+
+    // Project: Fauna do Brasil
+    'fauna_do_brasil' => 'Fauna do Brasil',
+    'fauna_do_brasil_description' => 'Coleções Fauna do Brasil é empreendedorismo sustentável que vende bichinhos de pelúcia da Mata Atlântica.',
+
+    // Project: Mais Futuro
+    'mais_futuro' => 'Mais Futuro',
+    'mais_futuro_description' => 'Trabalho desenvolvido entre agosto de 2020 à abril de 2022 na empresa Mais Futuro como Designer UI/UX e Desenvolvedora de Software.',
+
+    // Project: Pantera Motors
+    'pantera_motors' => 'Pantera Motors',
+    'pantera_motors_description' => 'Redesign de e-commerce de venda e compra de carros.',
+
+    // Project: skoob
+    'skoob' => 'Skoob redesign',
+    'skoob_description' => 'Por ser uma usuária antiga do Skoob, decidi aplicar as heurísticas de Nielsen no formulário da bio na tela de perfil do usuário.',
+    
+    // Project: sorlaDB
+    'sorladb' => 'Sorla DB',
+    'sorladb_description' => 'Projeto OpenSource de biblioteca Javascript para banco de dados temporários criados para uso em ambiente de navegador.',
+];
+
