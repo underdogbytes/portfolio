58bfa3c30b6914351ec97a63fe7bd07a025bf871
bea <beatriztattooer@gmail.com>
2024-09-17 13:18:46 -0300

    :recycle: refactor[portfolio]: componentize header section
    - Refactored the header section into a standalone component

---
+++ resources/views/components/portfolio/header.blade.php
@@ -0,0 +1,15 @@
+<div class="header-box__title">
+    <h1>{{ $project['name'] }}</h1>
+
+    @if(isset($project['role']))
+        <p>
+            @foreach($project['role'] as $role)
+            {!! __('projects.roles_title') !!}
+            <br>
+            <span>
+                {!! __($role) !!} @if(!$loop->last), @endif
+            </span>
+            @endforeach
+        </p>
+    @endif
+</div>
