be79dbdd297567209860cbb6e81e5a98992e85b4
bea <beatriztattooer@gmail.com>
2024-09-17 13:13:32 -0300

    :recycle: refactor[component]: figma
    - lang

--- resources/views/components/portfolio/figma.blade.php
+++ resources/views/components/portfolio/figma.blade.php
@@ -1,5 +1,4 @@
 <div class="m-5-auto">
-  <h2>Figma</h2>
-
+  <h2>{!! __('projects.tools_figma') !!}</h2>
   <iframe class="portfolio__proto_figma" src="{{ $link }}" allowfullscreen></iframe>
 </div>
