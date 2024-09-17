5038f4d0b9f3d53ce22a9e1364b409cc9481f607
bea <beatriztattooer@gmail.com>
2024-09-17 13:33:18 -0300

    :bug: fix[portfolio] : research component
    Moved isset to parent

--- resources/views/components/portfolio/research.blade.php
+++ resources/views/components/portfolio/research.blade.php
@@ -1,6 +1,4 @@
-@if(isset($project['body']['research']) && $project['body']['research'])
 <div class="m-5-auto">
     <h2>{!! __($project['body']['research']['title']) !!}</h2>
     {!! __($project['body']['research']['text']) !!}
 </div>
-@endif
