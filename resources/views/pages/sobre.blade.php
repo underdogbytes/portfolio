d0d8f86b8c92a772d1d90e04f2b469608d5d7596
bea <beatriztattooer@gmail.com>
2025-01-19 01:30:21 -0300

    feat[about-page]: adição "presença online"

--- resources/views/pages/sobre.blade.php
+++ resources/views/pages/sobre.blade.php
@@ -5,4 +5,24 @@
 <div class="ghost"></div>
 <x-about.intro />
 
+<div>
+  <div class="inner">
+    <h2>Minha Presença Online</h2>
+    <div>
+      <p>
+        <a href="https://www.patreon.com/c/btrz" target="_blank">>> Patreon</a>
+        <br>
+        Minha página do Patreon com conteúdo exclusivo baseado em solicitações de meus patronos.
+      </p>
+    </div>
+    <div>
+      <p>
+        <a href="https://www.instagram.com/bridgetocross/" target="_blank">>> Instagram</a>
+        <br>
+        Acompanhe minhas inspirações e referências cotidianas, além do meu trabalho.
+      </p>
+    </div>
+  </div>
+</div>
+
 @endsection
